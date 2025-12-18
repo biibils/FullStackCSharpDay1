dd7cb5b3a2bbff1703d29a922a21ebfa402e1e47
biibils <billy.sutawijaya.04@gmail.com>
2025-12-18 12:40:40 +0700

    testing

--- MiniPro1-Book/obj/Debug/net8.0/MiniPro1-Book.AssemblyInfo.cs
+++ MiniPro1-Book/obj/Debug/net8.0/MiniPro1-Book.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniPro1-Book")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e569c775a98235929b1a502aab534900c5af5e47")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniPro1-Book")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniPro1-Book")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
